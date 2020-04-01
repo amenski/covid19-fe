a49029ae9e90d64e5a4ea1b1f9962b60d05674f5
faahta <fasso143@gmail.com>
2020-04-02 00:42:53 +0200

    COV-ET-01 Register component added

---
+++ src/app/pages/register/register-user.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-register-user",
+  templateUrl: "register-user.component.html"
+})
+export class RegisterUserComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
