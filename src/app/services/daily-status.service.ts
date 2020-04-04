310da451c890d9fddba48c2176c0b23ec9bfaed9
faahta <fasso143@gmail.com>
2020-04-04 17:39:36 +0200

    COV-ET-02 Daily status service added

---
+++ src/app/services/daily-status.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Case} from "../models/case";
+import {BASE_URL} from "../helpers/constants";
+import {HttpClient} from "@angular/common/http";
+import {DailyStatus} from "../models/daily-status";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DailyStatusService {
+
+  constructor(private http: HttpClient) { }
+  getAllCaseStats(): Observable<DailyStatus[]>{
+    let dailyStatUrl = BASE_URL+"/v1/daily-status/all";
+    // let headers = {
+    //   'Access-Control-Allow-Origin': true
+    // };
+    // @ts-ignore
+    return this.http.get<DailyStatus[]>(dailyStatUrl);
+  }
+}
